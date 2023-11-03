a63e11ab459e5c30bdf6958efc4541a6f8bd410a
HARSHGUPTA2 <97681364+HARSHGUPTA2@users.noreply.github.com>
2023-11-03 18:47:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var lastname = "gupta"
+var firstname = "harsh"
+var a = "my first console project"
+var age = "13"
+var standard = "8th"
+var divison = "innovaters"
+var add = firstname + lastname
+var fullname =firstname.concat(lastname);
+console.log(a);
+console.log("my name is - " + add);
+console.log("I am " + age + "years old");
+console.log("I am studing in " + standard + " standard");
+console.log("my divison is " + divison);
+console.log(fullname);
